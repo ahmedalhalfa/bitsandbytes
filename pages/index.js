e4e054f699f54632a8d00800568769f94ef67dfa
ahmed alhalfa <ah.alhalfa@gmail.com>
2024-10-27 20:16:22 +0300

    finished the blog

---
+++ pages/index.js
@@ -0,0 +1,80 @@
+import Head from "next/head";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+import BlogHeader from "../Components/BlogHeader";
+import { getAllBlogPosts, getAllTopics } from "../Lib/Data";
+
+export const getStaticProps = () => {
+  const allBlogs = getAllBlogPosts();
+  const allTopics = getAllTopics();
+  return {
+    props: {
+      blogs: allBlogs,
+      topics: allTopics,
+    },
+  };
+};
+
+export default function Home({ blogs, topics }) {
+  return (
+    <>
+      <Head>
+        <title>Bits & Bytes with Halfa 🚀</title>
+        <meta name="title" content="Bits & Bytes with Halfa 🚀" />
+        <meta
+          name="description"
+          content="Tech blogs and articles on various topics related to Software Development"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://blogs.halfa.site/" />
+        <meta property="og:title" content="Bits & Bytes with Halfa 🚀" />
+        <meta
+          property="og:description"
+          content="Tech blogs and articles on various topics related to Software Development"
+        />
+        <meta
+          property="og:image"
+          content="https://i.ibb.co/Xy6tpc4/Screenshot-2024-10-27-201204.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://blogs.halfa.site/" />
+        <meta property="twitter:title" content="Bits & Bytes with Halfa 🚀" />
+        <meta
+          property="twitter:description"
+          content="Tech blogs and articles on various topics related to Software Development"
+        />
+        <meta
+          property="twitter:image"
+          content="https://i.ibb.co/Xy6tpc4/Screenshot-2024-10-27-201204.png"
+        />
+      </Head>
+
+      <div className="min-h-screen relative bg-white dark:bg-gray-900">
+        <Navbar topics={topics} />
+        <Header />
+
+        <div className="px-0.5 md:px-7 pb-14 pt-6 mx-auto">
+          <div className="flex flex-wrap">
+            {blogs &&
+              blogs.map(
+                (blog) =>
+                  blog.data.isPublished && (
+                    <BlogHeader
+                      key={blog.data.Id}
+                      data={blog.data}
+                      content={blog.content}
+                      readTime={blog.readTime.text}
+                    />
+                  )
+              )}
+          </div>
+        </div>
+
+        <Footer />
+      </div>
+    </>
+  );
+}
